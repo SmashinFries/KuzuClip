4d4675a8175543c2a92ac152fb16457d379d264b
Smashinfries <jus10luka@gmail.com>
2023-12-16 16:37:28 -0600

    init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { InferenceSession } from 'onnxruntime-react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [encoder, setEncoder] = useState<InferenceSession>();
+  const [decoder, setDecoder] = useState<InferenceSession>();
+
+  const loadModels = async () => {
+    const e = await InferenceSession.create('./models/encoder.onnx');
+    const d = await InferenceSession.create('./models/decoder.onnx');
+    setEncoder(e);
+    setDecoder(d);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Encoder: {encoder ? 'loaded' : 'not loaded'}</Text>
+      <Text>Decoder: {decoder ? 'loaded' : 'not loaded'}</Text>
+      <Button title="Load models" onPress={loadModels} />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/inference.ts
@@ -0,0 +1,25 @@
+import { InferenceSession } from "onnxruntime-react-native";
+import { useEffect, useState } from "react";
+
+const useSAM = () => {
+    const [mask_threshold, setMaskThreshold] = useState(0.0);
+    const [img_size, setImgSize] = useState(1024);
+    const [pixel_mean, setPixelMean] = useState([123.675, 116.28, 103.53]);
+    const [pixel_std, setPixelStd] = useState([58.395, 57.12, 57.375]);
+    const [encoder, setEncoder] = useState<InferenceSession>();
+    const [decoder, setDecoder] = useState<InferenceSession>();
+
+    // const encoder: InferenceSession = await InferenceSession.create('models/encoder.onnx');
+    // const decoder: InferenceSession = await InferenceSession.create('models/decoder.onnx');
+
+    const predict = (point_coords?:number[], point_labels?:number[]) => {
+
+    };
+
+    useEffect(() => {
+        if (!encoder && !decoder) {
+            InferenceSession.create('models/encoder.onnx').then((encoder) => setEncoder(encoder));
+            InferenceSession.create('models/decoder.onnx').then((decoder) => setDecoder(decoder));
+        }
+    },[])
+};
